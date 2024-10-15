4ca4d5a582cfa704350f6f2528b489aa151b20c9
Abhijith Sriram <abhijith.sriram@marinejetpower.com>
2024-10-15 11:03:06 +0200

    Split the output build into two directories

--- build.bat
+++ build.bat
@@ -1,43 +1,49 @@
 @echo off
 SETLOCAL EnableDelayedExpansion
 cls
-IF EXIST ".\out\"  (
-    echo Deleting the out folder for a clean build
-    rmdir .\out /q /s
-)
 
-SET CMAKE_ARGS=-S %~dp0 -B %~dp0\out\build -DAPPL_TO_BUILD
-
-@REM Get the application name to build
-IF [%~1] == [] (
-    goto display_help
-) ELSE (
-    SET CMAKE_ARGS=!CMAKE_ARGS!=%~1
-)
+SET CMAKE_ARGS=-S %~dp0
 
 IF [%~2] == [] (
     goto :display_help
 ) ELSE IF "%~2" EQU "EMULATOR" (
     @REM Building for emulator, output will be a dll file
-    SET CMAKE_ARGS=!CMAKE_ARGS! -DBUILD_TARGET=EMULATOR
-    goto :build_target
+    SET BUILD_DIR=%~dp0/out/emulator
+    SET CMAKE_ARGS=!CMAKE_ARGS! -B !BUILD_DIR!
+    SET CMAKE_ARGS=!CMAKE_ARGS! -DBUILD_TARGET=EMULATOR -DAPPL_TO_BUILD
 
 ) ELSE IF "%~2" EQU "MCU" (
     @REM Building to flash the arduino, output will be .hex file
     @REM We need to have a different build generator when cross-compiling else the compiler settings
     @REM defaults to msvc. If this happens then we cannot build binary for arduino, this is why we use -G argument
-    SET CMAKE_ARGS=!CMAKE_ARGS! -DBUILD_TARGET=MCU -G"MinGW Makefiles"
-    goto :build_target
+    SET BUILD_DIR=%~dp0/out/flash
+    SET CMAKE_ARGS=!CMAKE_ARGS! -B !BUILD_DIR!
+    SET CMAKE_ARGS=!CMAKE_ARGS! -DBUILD_TARGET=MCU -G"MinGW Makefiles" -DAPPL_TO_BUILD
     
 ) ELSE (
     goto :display_help
 )
 
+@REM Get the application name to build
+IF [%~1] == [] (
+    goto :display_help
+) ELSE (
+    echo Application to build: %~1
+    SET CMAKE_ARGS=!CMAKE_ARGS!=%~1
+    goto :build_target
+)
+
+@REM Cleaning the out folder
+IF EXIST ".\out\!BUILD_DIR!"  (
+    echo Deleting the out folder for a clean build
+    rmdir .\out /q /s
+)
+
 :build_target
-    echo Generating the build files
+    echo Generating the build files !CMAKE_ARGS!
     cmake !CMAKE_ARGS!
     echo Building the target
-    cmake --build %~dp0\out\build
+    cmake --build !BUILD_DIR!
     goto :eof
 
 :display_help
